31808818ba87dd9631ba7e248d47069125df0d36
(Bill) Yuchen Lin <yuchen.lin@usc.edu>
2023-03-07 14:33:31 -0800

    add files

--- README.md
+++ README.md
@@ -6,20 +6,20 @@ This is a template repository for Python-based research projects.
 
 1. [Create a new repository](https://github.com/allenai/python-project-template/generate) from this template with the desired name of your Python project.
 
-2. Change the name of the `my_project` directory to the name of your repo / Python project.
+2. Change the name of the `rank_outputs` directory to the name of your repo / Python project.
 
-3. Replace all mentions of `my_project` throughout this repository with the new name.
+3. Replace all mentions of `rank_outputs` throughout this repository with the new name.
 
-    On OS X, a quick way to find all mentions of `my_project` is:
+    On OS X, a quick way to find all mentions of `rank_outputs` is:
 
     ```bash
-    find . -type f -not -path './.git/*' -not -path ./README.md -not -path './docs/build/*' -not -path '*__pycache__*' | xargs grep 'my_project'
+    find . -type f -not -path './.git/*' -not -path ./README.md -not -path './docs/build/*' -not -path '*__pycache__*' | xargs grep 'rank_outputs'
     ```
 
-    There is also a one-liner to find and replace all mentions `my_project` with `actual_name_of_project`:
+    There is also a one-liner to find and replace all mentions `rank_outputs` with `actual_name_of_project`:
 
     ```bash
-    find . -type f -not -path './.git/*' -not -path ./README.md -not -path './docs/build/*' -not -path '*__pycache__*' -exec sed -i '' -e 's/my_project/actual_name_of_project/' {} \;
+    find . -type f -not -path './.git/*' -not -path ./README.md -not -path './docs/build/*' -not -path '*__pycache__*' -exec sed -i '' -e 's/rank_outputs/actual_name_of_project/' {} \;
     ```
 
 4. Update the README.md.
---
+++ rank_outputs/main.py
@@ -0,0 +1,106 @@
+from transformers import AutoTokenizer, AutoConfig
+from utils import ModelBase, DataCollatorForMultipleChoice
+from torch.utils.data import DataLoader
+import torch
+
+
+def init_model(model_name_or_path="google/flan-t5-small"):
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+    config = AutoConfig.from_pretrained(model_name_or_path)
+    model = ModelBase.from_config(
+            config=config,
+            model_name_or_path=model_name_or_path,
+            parallelize=False
+        )
+    return model, tokenizer
+
+
+def convert_features(tokenizer, data): 
+    input_texts = data["input_texts"]
+    answer_choices_texts = data["answer_choices_texts"]
+    target_texts = data["target_texts"]
+    bs = 1
+    padding = "max_length" 
+    max_length = 64
+    
+    tokenized_inputs = tokenizer(
+                input_texts,
+                padding=padding,
+                max_length=max_length,
+                truncation=True,
+                add_special_tokens=False,
+            )
+
+    tokenized_targets = [
+        tokenizer(
+            option,
+            # padding is on the right here.
+            padding=True,
+            max_length=max_length,
+            truncation=True,
+        )
+        for option in answer_choices_texts
+    ]
+    
+
+    features = {
+                k: [
+                    [elem for _ in range(len(tokenized_targets[idx]["input_ids"]))]
+                    for idx, elem in enumerate(v)
+                ]
+                for k, v in tokenized_inputs.items()
+            }
+
+    features["labels"] = [
+        tokenized_targets[idx]["input_ids"]
+        for idx in range(bs)
+    ]
+    features["labels_attention_mask"] = [
+        tokenized_targets[idx]["attention_mask"]
+        for idx in range(bs)
+    ]
+    features["targets"] = [
+        answer_choices_texts[idx].index(t)
+        for idx, t in enumerate(target_texts)
+    ]
+    return features
+
+def get_example_data():
+    input_texts = [
+        "Answer this question: I am black when you buy me, red when you use me. When I turn white, you know it's time to trow me away. What am I? ",
+        "Answer this question: I have a long tail that I let fly. Every time I go through a gap, I leave a bit of my tail in the trap. What am I?", 
+        "Compose a sentence that contains all given concepts: book read boy chair sit", 
+    ]    
+    answer_choices_texts = [
+        ["rose flower", "ink", "charcoal", "fruit", "shoe"],
+        ["monkey", "basketball", "fishing pole", "comet", "needle"],
+        ["A boy read chair that sits on a book.", "A boy sits on a chair and read a book.", "A book reads a boy that sits on a chair."],
+    ]
+    truths = [] # optional 
+    
+    return {"input_texts": input_texts, "answer_choices_texts": answer_choices_texts, "truths": truths}
+
+def main():
+    model, tokenizer = init_model("google/flan-t5-small")
+    data = get_example_data()
+    features = convert_features(tokenizer, data)
+    
+    
+    data_collator = DataCollatorForMultipleChoice(
+                tokenizer, pad_to_multiple_of=None, padding=True, max_length=64
+                )
+    eval_dataloader = DataLoader([features], collate_fn=data_collator, batch_size=1)
+    model.eval()
+
+    # features = [features]
+    # batch = data_fomratter(tokenizer, features) 
+
+    for batch in eval_dataloader:
+        batch = {
+            k: v.squeeze() if k!="targets" else v.view(1)
+            for k, v in batch.items()
+        }
+        with torch.no_grad():
+            predictions, seq_log_prob = model(batch)
+            print(predictions) 
